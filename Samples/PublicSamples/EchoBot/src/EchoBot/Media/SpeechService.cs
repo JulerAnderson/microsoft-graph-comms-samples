fca39ce19150f42b543aebc95430cc2c80118692
Juler Anderson Hermitano Castro <juleranderson@gmail.com>
2025-04-02 17:47:43 -0500

    Add TextToSpeech invocation on session start in SpeechService

--- Samples/PublicSamples/EchoBot/src/EchoBot/Media/SpeechService.cs
+++ Samples/PublicSamples/EchoBot/src/EchoBot/Media/SpeechService.cs
@@ -192,6 +192,7 @@ namespace EchoBot.Media
 
                 _recognizer.SessionStarted += async (s, e) =>
                 {
+                    TextToSpeech(SendMessageToWatsonAsync("Hola"));
                     _logger.LogInformation("\nSession started event.");
                     _logger.LogInformation($"[WATSONXAI] Creo que aquí es cuando invocas al bot");
                     // await SpeakRawTextAsync("Buenas tardes, soy TGI, ¿en qué puedo ayudarle hoy?");
